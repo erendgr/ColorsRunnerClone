d7c1b0027d1b40183952a3ad1a9c1c74e7637604
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-06 15:10:24 +0300

    Game Manager Added

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Application.targetFrameRate = 60;
+        }
+    }
+}
