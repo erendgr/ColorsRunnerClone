3a7e0b801815e5926f7b3eb6961e78d4dc4392a2
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-07 13:47:13 +0300

    Drone Physic System Improvement

---
+++ Assets/Scripts/Controllers/RunnerArea/DronePoolPhysicController.cs
@@ -0,0 +1,37 @@
+﻿using Managers;
+using UnityEngine;
+
+namespace Controllers.RunnerArea
+{
+    public class DronePoolPhysicController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region SerializeField Variables
+
+        [SerializeField] private DronePoolManager manager;
+
+        #endregion
+
+        #region Private Variables
+
+        private int _triggerCache;
+
+        #endregion
+
+        #endregion
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                manager.SelectedArea = true;
+                return;
+            }
+
+            if (!other.CompareTag("Collected") || _triggerCache > 0) return; //onceden data olarak alınabilir
+            manager.SetTruePoolColor(other.transform.parent.GetComponent<CollectableManager>().ColorState);
+            _triggerCache++;
+        }
+    }
+}
