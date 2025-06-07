27a6530475da068c8caf7f84665e87d3bf751efa
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-07 13:15:13 +0300

    Mini Game Manager Added

---
+++ Assets/Scripts/Managers/MiniGameManager.cs
@@ -0,0 +1,119 @@
+﻿using Signals;
+using TMPro;
+using UnityEngine;
+
+namespace Managers
+{
+    public class MiniGameManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private GameObject pointerImage;
+        [SerializeField] private TextMeshProUGUI bonusScore;
+
+        #endregion
+
+        #region Private Variables
+
+        private int _claimFactor;
+        private int _stackScore;
+
+        #endregion
+
+        #endregion
+
+        #region Event Subscriptions
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            ScoreSignals.Instance.onGetScore += OnGetScore;
+            ScoreSignals.Instance.onSendFinalScore += OnSendFinalScore;
+        }
+
+        private void UnsubscribeEvents()
+        {
+            ScoreSignals.Instance.onGetScore -= OnGetScore;
+            ScoreSignals.Instance.onSendFinalScore -= OnSendFinalScore;
+        }
+
+        private void OnDisable()
+        {
+            UnsubscribeEvents();
+        }
+
+        #endregion
+
+        private void Update()
+        {
+            PlayPointerAnim();
+        }
+
+        private void PlayPointerAnim()
+        {
+            pointerImage.transform.localEulerAngles = new Vector3(0, 0, Mathf.PingPong(Time.time * 30, 30f) - 15f);
+            pointerImage.transform.localPosition = new Vector3(Mathf.PingPong(Time.time * 660, 660) - 330,
+                Mathf.PingPong(Time.time * 74, 37) - 67f, 0);
+            SetClaimFactor();
+            SetBonusScore();
+        }
+
+        private void SetClaimFactor()
+        {
+            var pointerEulerAnglesZ = pointerImage.transform.eulerAngles.z;
+            if (pointerEulerAnglesZ < 15)
+            {
+                if (pointerEulerAnglesZ > 10)
+                {
+                    _claimFactor = 2;
+                }
+                else if (pointerEulerAnglesZ < 3)
+                {
+                    _claimFactor = 5;
+                }
+                else
+                {
+                    _claimFactor = 3;
+                }
+            }
+            else
+            {
+                if (pointerEulerAnglesZ < 350)
+                {
+                    _claimFactor = 2;
+                }
+                else if (pointerEulerAnglesZ > 357)
+                {
+                    _claimFactor = 5;
+                }
+                else
+                {
+                    _claimFactor = 3;
+                }
+            }
+        }
+
+        private void SetBonusScore()
+        {
+            bonusScore.text = (_stackScore * _claimFactor).ToString();
+        }
+
+        private void OnGetScore(int score)
+        {
+            _stackScore = score;
+        }
+
+        private void OnSendFinalScore()
+        {
+            int bonusScoreInt = _stackScore * _claimFactor;
+            StackSignals.Instance.onSetPlayerScale(0.1f * bonusScoreInt);
+            ScoreSignals.Instance.onSetScore?.Invoke(bonusScoreInt);
+        }
+    }
+}
