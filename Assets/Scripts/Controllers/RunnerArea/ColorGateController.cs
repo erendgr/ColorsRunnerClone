3478bf6cf6ac6c89fe93f31f43062fb7bf1db547
erendgr <49555544+erendgr@users.noreply.github.com>
2025-06-07 13:46:33 +0300

    Gate Controller Added

---
+++ Assets/Scripts/Controllers/RunnerArea/ColorGateController.cs
@@ -0,0 +1,70 @@
+﻿using Data.UnityObject;
+using Datas.ValueObjects;
+using Enums;
+using Signals;
+using UnityEngine;
+
+namespace Controllers.RunnerArea
+{
+    public class ColorGateController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Public Variables
+
+        private ColorEnum ColorEnum
+        {
+            get => colorEnum;
+            set
+            {
+                this.colorEnum = value;
+                SetColor();
+            }
+        }
+
+        #endregion
+
+        #region SerializeField Variables
+
+        [SerializeField] private ColorEnum colorEnum;
+
+        #endregion
+
+        #region Private Variables
+
+        private ColorData _colorData;
+
+        #endregion
+
+        #endregion
+
+        private void Awake()
+        {
+            _colorData = GetColorData();
+        }
+
+        private void Start()
+        {
+            ColorEnum = colorEnum;
+        }
+
+        private ColorData GetColorData() => Resources.Load<CD_Color>("Data/CD_Color").colorData;
+
+        private void SetColor()
+        {
+            if (ColorEnum == ColorEnum.Rainbow)
+            {
+                gameObject.GetComponent<Renderer>().material = _colorData.RainbowGageMaterial;
+            }
+            else gameObject.GetComponent<Renderer>().material.color = _colorData.color[(int)ColorEnum];
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                StackSignals.Instance.ColorType?.Invoke(ColorEnum);
+            }
+        }
+    }
+}
